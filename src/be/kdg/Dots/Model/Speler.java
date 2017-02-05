889025c2603fd1bf0050e817f64ce263677e7703
goffincedric <cedric.goffin@student.kdg.be>
2017-02-05 14:57:40 +0100

    Project structure: Classes added

---
+++ src/be/kdg/Dots/Model/Speler.java
@@ -0,0 +1,8 @@
+package be.kdg.Dots.Model;
+
+/**
+ * @author Cédric Goffin
+ * @version 1.0 5/02/2017 14:56
+ */
+public class Speler {
+}
---
+++ src/be/kdg/MainConsole.java
@@ -0,0 +1,11 @@
+package be.kdg;
+
+/**
+ * @author Cédric Goffin
+ * @version 1.0 5/02/2017 14:55
+ */
+public class MainConsole {
+    public static void main(String[] args) {
+
+    }
+}
