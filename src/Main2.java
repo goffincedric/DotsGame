5884c587b57dff5f5c10cd605a4c78d8c87aa45f
goffincedric <cedric.goffin@student.kdg.be>
2017-02-05 14:20:49 +0100

    test

---
+++ src/Main2.java
@@ -0,0 +1,9 @@
+/**
+ * @author Cédric Goffin
+ * @version 1.0 5/02/2017 14:20
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
