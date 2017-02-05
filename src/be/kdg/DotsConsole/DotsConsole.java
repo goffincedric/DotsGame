cf176b2e9692bc5bed11f731d35dbe69347a8fed
goffincedric <cedric.goffin@student.kdg.be>
2017-02-05 14:58:54 +0100

    Project structure: Classes added

---
+++ src/be/kdg/DotsConsole/DotsConsole.java
@@ -0,0 +1,9 @@
+package be.kdg.DotsConsole;
+
+/**
+ * @author Cédric Goffin
+ * @version 1.0 5/02/2017 14:58
+ */
+public class DotsConsole {
+
+}
--- src/be/kdg/MainConsole.java
+++ src/be/kdg/MainConsole.java
@@ -1,11 +1,14 @@
 package be.kdg;
 
+import be.kdg.Dots.Dots;
+import be.kdg.DotsConsole.DotsConsole;
+
 /**
  * @author Cédric Goffin
  * @version 1.0 5/02/2017 14:55
  */
 public class MainConsole {
     public static void main(String[] args) {
-
+        DotsConsole dotsGame = new DotsConsole();
     }
 }
