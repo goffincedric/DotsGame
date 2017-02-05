b949d87389da10e4ae655d95ad7e872f48f7c3f1
goffincedric <cedric.goffin@student.kdg.be>
2017-02-05 14:37:30 +0100

    Project structure added

---
+++ src/be/kdg/Main.java
@@ -0,0 +1,15 @@
+package be.kdg;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Cédric Goffin
+ * @version 1.0 5/02/2017 14:36
+ */
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
