e1ae76ce5a209b6328409e457d1ac71beb1a1116
ThomasVerhoeven <thomas.verhoeven@student.kdg.be>
2017-02-05 14:14:35 +0100

    new

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Thomas Verhoeven
+ * @version 1.0 5/02/2017 14:12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
