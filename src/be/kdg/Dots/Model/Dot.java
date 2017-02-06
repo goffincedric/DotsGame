be6a7bfa8682c8a0919c319cf145057f29df4846
ThomasVerhoeven <thomas.verhoeven@student.kdg.be>
2017-02-06 19:48:34 +0100

    new

--- src/be/kdg/Dots/Model/Dot.java
+++ src/be/kdg/Dots/Model/Dot.java
@@ -1,9 +1,26 @@
 package be.kdg.Dots.Model;
 
+import javafx.scene.shape.Circle;
+
+import java.awt.*;
+
 /**
  * @author Cédric Goffin
  * @version 1.0 5/02/2017 14:50
  */
 public class Dot {
+    private Kleuren color;
+    private int rijIndex; //bepaalt colomindex in array of buttons;
+    private int kolomIndex; //bepaalt rijindex in array of buttons;
+    Circle shape;
+    //nog niet zeker over die kolom en rijindex
 
+
+    public Dot() {
+        shape=new Circle();
+        //rijindex en kolomindex in array bepalen
+        ///color bepalen random
+
+
+    }
 }
