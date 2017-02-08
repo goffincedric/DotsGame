da31d37a2027f74b8478dcd225a4bef2ebf8e145
goffincedric <cedric.goffin@student.kdg.be>
2017-02-08 12:19:45 +0100

    Project structure: Structure reformat

---
+++ src/be/kdg/DotsConsole2/DotsConsole.java
@@ -0,0 +1,54 @@
+package be.kdg.DotsConsole2;
+
+
+/**
+ * @author Cédric Goffin
+ * @version 1.0 5/02/2017 14:58
+ */
+public class DotsConsole {
+   private final int MAXRIJ = 7;
+   private final int MAXKOLOM = 7;
+   private DotConsole[][] speelveld;
+
+    public DotsConsole() {
+        speelveld = new DotConsole[MAXRIJ][MAXKOLOM];
+
+       for(int i = 0; i < speelveld.length ;i++){
+           for (int j = 0; j < speelveld[i].length; j++){
+               speelveld[i][j] = new DotConsole(i,j);
+           }
+
+       }
+    }
+
+    public void printveld() {
+
+
+        for(int k=0; k <7; k++){
+            System.out.print(" "+ k + "\t");
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < speelveld.length; i++) {
+
+            for (int j = 0; j < speelveld[i].length; j++) {
+
+                System.out.print("*" + speelveld[i][j].getKleur() + "\t");
+
+            }
+            System.out.println("\t" + i);
+            System.out.println();
+
+        }
+    }
+
+
+    public DotConsole[][] getSpeelveld() {
+        return speelveld;
+    }
+
+    public DotConsole getDotUitSpeelveld(int rij, int kolom){
+        return speelveld[rij][kolom];
+    }
+}
---
+++ src/be/kdg/DotsConsole2/LijnConsole.java
@@ -0,0 +1,71 @@
+package be.kdg.DotsConsole2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Thomas Verhoeven
+ * @version 1.0 6/02/2017 22:01
+ */
+public class LijnConsole extends DotsConsole {
+
+    //vraag user naar kolom en rij
+    //blijf dit doen in een loop
+    //vergelijk steeds de  dots
+
+    private List<DotConsole> lijn;
+    private int aantalDots;
+    private String lijnKleur;
+
+
+    public LijnConsole() {
+        lijn = new ArrayList<>();
+        this.aantalDots = 0;
+    }
+
+    public List<DotConsole> getLijn() {
+        return lijn;
+    }
+
+
+    public void addDot(DotConsole andereDot) {
+
+
+
+        if (lijn.size() == 0) {
+            lijn.add(0,andereDot);
+
+
+
+        } else {
+
+            if (lijn.get(lijn.size()-1).getKleur().equals(andereDot.getKleur())) {
+                if ((lijn.get(lijn.size()-1).getKolomIndex() - 1 == andereDot.getKolomIndex()) || (lijn.get(lijn.size()-1).getKolomIndex() == andereDot.getKolomIndex()) || (lijn.get(lijn.size()-1 ).getKolomIndex() + 1 == andereDot.getKolomIndex())) {
+                    if ((lijn.get(lijn.size()-1).getRijIndex() - 1 == andereDot.getRijIndex()) || (lijn.get(lijn.size()-1).getRijIndex() == andereDot.getRijIndex()) || (lijn.get(lijn.size()-1).getRijIndex() + 1 == andereDot.getRijIndex())) {
+
+                        lijn.add(lijn.size(),andereDot);
+
+
+                    }
+                }
+            } else {
+                System.out.println("Bollen hebben niet dezelfde kleur of liggen niet naast elkaar");
+            }
+        }
+
+
+
+    }
+
+
+
+
+    public String printList() {
+
+        for (DotConsole dotConsole : lijn) {
+            return String.format("Kleur: " + dotConsole.getKleur() + "\nIndex [Rij|Kolom]: " + dotConsole.getRijIndex()) + dotConsole.getKolomIndex();
+
+        }
+        return "";
+    }
+}
