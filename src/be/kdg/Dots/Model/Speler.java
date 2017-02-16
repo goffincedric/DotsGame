821529c6d094eb734b87a8e3e85a74ec0ecad0e6
ThomasVerhoeven <thomas.verhoeven@student.kdg.be>
2017-02-16 12:33:44 +0100

    StartView button shit

--- src/be/kdg/Dots/Model/Speler.java
+++ src/be/kdg/Dots/Model/Speler.java
@@ -1,11 +1,16 @@
 package be.kdg.Dots.Model;
 
+import java.util.HashMap;
+
 /**
  * @author Cédric Goffin
  * @version 1.0 5/02/2017 14:56
  */
 public class Speler {
     private String naam;
+    private int highScore;
+    private HashMap<String, Integer> Highscores;
+    private int score;
 
     public Speler() {
 
@@ -24,4 +29,21 @@ public class Speler {
     }
 
 
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void nieuweHighscore() {
+        if (!Highscores.containsKey(getNaam().toLowerCase())) {
+            Highscores.put(getNaam().toLowerCase(), super.lijn.bepaalScore());
+        }
+        if (Highscores.get(getNaam().toLowerCase()) < (super.lijn.bepaalScore())) {
+            Highscores.put(getNaam().toLowerCase(), super.lijn.bepaalScore());
+        }
+
+    }
 }
