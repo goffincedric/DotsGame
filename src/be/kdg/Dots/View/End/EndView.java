76aa5618906381650278656ce83709a100f4a9d5
ThomasVerhoeven <thomas.verhoeven@student.kdg.be>
2017-02-19 21:04:09 +0100

    StartView Basics Endview

--- src/be/kdg/Dots/View/End/EndView.java
+++ src/be/kdg/Dots/View/End/EndView.java
@@ -1,8 +1,11 @@
 package be.kdg.Dots.View.End;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * @author Cédric Goffin
@@ -20,16 +23,48 @@ public class EndView extends BorderPane {
     private Button btnHome;
     private Button btnRestart;
 
+    private HBox levelBox;
+    private HBox scoreBox;
+    private HBox buttonBox;
+    private VBox bottomBox;
+
     public EndView() {
         this.initialiseNodes();
         this.layoutNodes();
     }
 
     private void initialiseNodes() {
+        newHighScore = new Label("10000");
+        levelTekst = new Label("Level: ");
+        scoreTekst = new Label("Score: ");
+        huidigLevel = new Label("10");
+        huidigeScore = new Label("250");
+        btnHome = new Button("Home");
+        btnRestart = new Button("Restart");
+        levelBox = new HBox();
+        scoreBox = new HBox();
+        buttonBox = new HBox();
+        bottomBox = new VBox();
 
     }
 
     private void layoutNodes() {
+
+        levelBox.getChildren().addAll(levelTekst, huidigLevel);
+        scoreBox.getChildren().addAll(scoreTekst, huidigeScore);
+        buttonBox.getChildren().addAll(btnHome, btnRestart);
+        bottomBox.getChildren().addAll(levelBox,scoreTekst, buttonBox);
+
+        
+        newHighScore.setAlignment(Pos.CENTER);
+        levelBox.setAlignment(Pos.CENTER);
+        scoreTekst.setAlignment(Pos.CENTER);
+        buttonBox.setAlignment(Pos.CENTER);
+        bottomBox.setAlignment(Pos.CENTER);
+
+        this.setTop(newHighScore);
+        this.setBottom(bottomBox);
+
 
     }
 }
